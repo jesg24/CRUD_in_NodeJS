f88ebd0506b78f4e9c2d9678e53f8f28d495975a
jesg24 <esguerrajulien@gmail.com>
2020-09-24 20:20:11 +0800

    Successfully connected to the database using Mysql.

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const app = express();
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'inventory'
+});
+
+connection.connect(function(error){
+    if(!!error) console.log(error);
+    else console.log('Database Connected!');
+});
 
 //route for home page
 app.get('/',(req, res) => {
